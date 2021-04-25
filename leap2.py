97901b6173e6bb12c057704b8bd40749b71b180d
aoumar153 <63009456+aoumar153@users.noreply.github.com>
2021-04-25 16:16:24 -0700

    Add files via upload

--- leap2.py
+++ leap2.py
@@ -1,15 +1,21 @@
 def main():
-    year = input("Input the year you want to check: ")
+    while(True):
+        year = input("Input the year you want to check: ")
+    
+        if(year.isdigit() == True and int(year) >= 0):
+            break
+        else:
+            print("wront input buddy has to numbers and not negative")
     print(year)
     if(int(year)% 4 == 0):
         if(int(year)%100 ==0):
             if(int(year)% 400 == 0):
                 print('This is a leap year')
             else: 
-                print('not a leap year') 
+                print('not a leap year')
         else: 
             print('This is a leap year')
     else: 
         print('not a leap year') 
-            
+        
 main()
